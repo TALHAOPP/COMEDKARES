2158af8e7ccf10ba752463c88b0f095ad7625a03
TALHAOPP <pateltalha675@gmail.com>
2026-02-03 13:18:18 +0530

    day 2 in comedkares

---
+++ src/day2_python_fundaments2.py
@@ -0,0 +1,26 @@
+#task1
+
+a = input("Enter your name : ")
+b = int(input("Enter your Age : "))
+c = (b+4)
+print(f"Based On your Current Age {b} your age would be {c} in 2030")
+print(f"Hey {a}, you will be {c} years old in 2030!")
+
+
+#task 2
+
+a = float (input("Whats the Total bill amount : "))
+b = int(input("How many people are there : "))
+c = (a/b)
+print(f"Total Bill : {a}. Each person pays:{c}")
+
+
+
+#task 3
+item = ("Laptop")
+quantity = int( 2 )
+price = float(499.99)
+stock = bool(True)
+print("item: Laptop, Qty: 2, Price: 499.99, Available: True")
+a = (quantity*price)
+print(f"the total cost is {a}")
