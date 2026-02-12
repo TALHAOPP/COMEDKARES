3e23d0b562c8163c7adeee4d14ebf8f36ef44b54
TALHAOPP <pateltalha675@gmail.com>
2026-02-12 15:41:32 +0530

    day 10 in comedkares

---
+++ src/day 10.py
@@ -0,0 +1,6 @@
+import pandas as pd
+data = {"CustomerID": [101,102,103,104,105,106],
+        "Name": ["amit","talha",None,"amaan","priya","ali"],
+        "Age": [25,None,30,35,48,90],
+        "City": ["Banglore","Mumbai","Kolkata","Meena","Delhi",None],
+        "PaymentMethod": ["UPI","Card","Cash","UPI",None,"Card"]}
